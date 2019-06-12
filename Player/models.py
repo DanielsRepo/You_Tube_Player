e34a46ac5e6d4ee0fc449ee27d6eb24c7547ce26
Daniel <48760555+DanielsRepo@users.noreply.github.com>
2019-06-12 12:02:51 +0300

    Update models.py

--- Player/models.py
+++ Player/models.py
@@ -3,16 +3,11 @@ from django.conf import settings
 
 from googleapiclient.discovery import build
 
-api_service_name = "youtube"
-api_version = "v3"
-scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
-
-# API_KEY = "AIzaSyBa1C4HQIOAkWwGHNAmi1MVRl7jEAO0QeM"
-API_KEY = "AIzaSyCmNAzZs2ga7mUhyqvCi8dVpQJ0NCIn4Dg"
 
 class Query(models.Model):
     query = models.CharField(max_length=200, blank=True)
 
+    
 class Video(models.Model):
     video_id = models.CharField(max_length=200, blank=True)
     video_title = models.CharField(max_length=200, blank=True)
@@ -26,55 +21,62 @@ class Video(models.Model):
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
 
-    def get_videos(self, q, max_results=15, order="relevance"):
-        if not q.strip(): 
+
+class YouTubeAPI:
+    def __init__(self):
+        self.youtube = build(
+            settings.API_SERVICE_NAME,
+            settings.API_VERSION,
+            developerKey=settings.API_KEY
+        )
+
+    def get_videos(self, query):
+        if not query.strip():
             return []
         else:
-            videos = Video.objects.filter(query__query = q)
+            videos = Video.objects.filter(query__query=query)
 
-            if videos.count() == 0:
-                youtube = build(api_service_name, api_version, developerKey=API_KEY)
-                search_response = youtube.search().list(
-                    q=q,
-                    type="video",
-                    order = order,
-                    part="id,snippet",
-                    maxResults=max_results
-                ).execute()
-
-                exist_videos = []
-                new_videos = []
-                if len(search_response.get("items", [])) > 0:
-
-                    q = Query.objects.create(query = q)
-
-                    for res in search_response.get("items", []):
-                        video = Video.objects.filter(video_id = res['id']['videoId']).first()
-
-                        if video != None:
-                            video.query.add(q)
-                            exist_videos.append(video)
-                        else:
-                            new_videos.append(Video(
-                                video_id = res['id']['videoId'],
-                                video_title = res['snippet']['title'],
-                                channel_title = res['snippet']['channelTitle'],
-                                date = res['snippet']['publishedAt'][0:10],
-                                preview = res['snippet']['thumbnails']['medium']['url']
-                            ))
-
-                    Video.objects.bulk_create(new_videos)
-
-                    video_ids = list(Video.objects.values_list('id', flat=True).order_by('-id'))[:15][::-1]
-                    query_videos = []
-
-                    for v_id in video_ids:
-                        q_v = Video.query.through(video_id = v_id, query_id = q.id)
-                        query_videos.append(q_v)
-
-                    Video.query.through.objects.bulk_create(query_videos)
-
-                return exist_videos + new_videos
-            else:
+            if videos:
                 return videos
+            else:
+                return self.search_videos(query)
 
+    def search_videos(self, query, max_results=15, order="relevance"):
+        search_response = self.youtube.search().list(
+            q=query,
+            type="video",
+            order=order,
+            part="id,snippet",
+            maxResults=max_results
+        ).execute()
+
+        exist_videos = []
+        new_videos = []
+
+        if len(search_response.get("items", [])) > 0:
+            q = Query.objects.create(query=query)
+
+            for res in search_response.get("items", []):
+                video = Video.objects.filter(video_id=res['id']['videoId']).first()
+
+                if video is not None:
+                    video.query.add(q)
+                    exist_videos.append(video)
+                else:
+                    new_videos.append(Video(
+                        video_id=res['id']['videoId'],
+                        video_title=res['snippet']['title'],
+                        channel_title=res['snippet']['channelTitle'],
+                        date=res['snippet']['publishedAt'][0:10],
+                        preview=res['snippet']['thumbnails']['medium']['url']
+                    ))
+
+            self.save_to_db(q, new_videos)
+
+        return exist_videos + new_videos
+
+    def save_to_db(self, query, new_videos):
+        old_videos_ids = list(Video.objects.values_list('id', flat=True))
+        Video.objects.bulk_create(new_videos)
+        new_created_videos = Video.objects.exclude(id__in=old_videos_ids)
+        query.video_set.add(*new_created_videos)

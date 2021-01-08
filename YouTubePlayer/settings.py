a438afedfca1c2b042c30afbbfd4748dc8e9cdd2
Daniel <mcdancher18@gmail.com>
2021-01-08 17:12:30 +0200

    added docker-compose

--- YouTubePlayer/settings.py
+++ YouTubePlayer/settings.py
@@ -59,18 +59,18 @@ TEMPLATES = [
 WSGI_APPLICATION = 'YouTubePlayer.wsgi.application'
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('POSTGRES_DB'),
-        'USER': env('POSTGRES_USER'),
-        'PASSWORD': env('POSTGRES_PASSWORD'),
-        'HOST': env('POSTGRES_HOST'),
-        'PORT': env('POSTGRES_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': env('POSTGRES_DB'),
+    #     'USER': env('POSTGRES_USER'),
+    #     'PASSWORD': env('POSTGRES_PASSWORD'),
+    #     'HOST': env('POSTGRES_HOST'),
+    #     'PORT': env('POSTGRES_PORT'),
+    # }
 }
 
 AUTH_PASSWORD_VALIDATORS = [

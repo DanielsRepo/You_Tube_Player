71426f7bff4d4336547904f86e6cca753e8de2e7
Daniel <mcdancher18@gmail.com>
2021-01-08 16:25:44 +0200

    added docker-compose

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!e:\files\daniel\univer\python\test\you_tube_player\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
